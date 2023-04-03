c9865bb0e8f0344438e31ad9de854e2566be85a1
smriazati <sarahriazati@gmail.com>
2023-04-03 13:40:03 -0400

    updates

--- components/Home/GridFlex.vue
+++ components/Home/GridFlex.vue
@@ -9,18 +9,16 @@
                         <div @mouseover="setActiveProject(item.item?.title, item.item?.location)"
                             @mouseout="clearActiveProject">
                             <div v-if="item.customTnail">
-                                <img cross-origin="anonymous" :src="$urlFor(item.customTnail?.url).format('webp').url()"
+                                <img :src="$urlFor(item.customTnail?.url).width(1500).format('webp').url()"
                                     :alt="item.customTnail?.altText">
                             </div>
                             <div v-else-if="item.item?.featured?.url">
-                                <img cross-origin="anonymous" :src="$urlFor(item.item.featured?.url).format('webp').url()"
+                                <img :src="$urlFor(item.item.featured?.url).width(1500).format('webp').url()"
                                     :alt="item.customTnail?.alt">
                             </div>
                             <div v-else-if="item.item?.firstProject?.img?.url">
-                                <img cross-origin="anonymous"
-                                    :src="$urlFor(item.item?.firstProject?.img?.url).format('webp').url()"
+                                <img :src="$urlFor(item.item?.firstProject?.img?.url).width(1500).format('webp').url()"
                                     :alt="item.item?.firstProject?.img?.altText">
-
                             </div>
                         </div>
 
--- components/Nav/Dropdown.vue
+++ components/Nav/Dropdown.vue
@@ -49,6 +49,9 @@ function collapseOtherDropdowns() {
 }
 
 function toggleDropdown(e) {
+    if (window.innerWidth < 800) {
+        collapseOtherDropdowns()
+    }
     const parent = e.target.closest('.dropdown');
     if (!parent) return;
     parent.classList.toggle('collapsed')
