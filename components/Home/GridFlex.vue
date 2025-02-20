90c7eb8e555278230b23e44c85328717e64e59b3
smriazati <sarahriazati@gmail.com>
2025-02-19 21:31:18 -0500

    refactors

--- components/Home/GridFlex.vue
+++ components/Home/GridFlex.vue
@@ -1,12 +1,14 @@
 <template>
     <div>
         <ul v-if="data?.grid" ref="grid" class="home-grid-items-blur">
-            <GridFlexItem v-for="item in data.grid" :key="item.slug?.current" :item="item" />
+            <GridFlexItem v-for="item in data.grid" :key="item.slug?.current" :item="item"
+                @set-active="setActiveProject" @clear-active="clearActiveProject" />
         </ul>
     </div>
 </template>
 
 <script setup>
+import { ref, watch, onMounted } from 'vue';
 import { useActiveProjectStore } from '@/stores/activeProject';
 import GridFlexItem from '@/components/Home/GridFlexItem.vue';
 
@@ -22,73 +24,85 @@ const query = `
               title,
               size
           },
-   _type == "projectGridItem" => {
-        "_type": item->_type,
-        "_id": item->_id,
-        "featured": item->featured.asset->{
-          "url": url,
-          "alt": altText
-        },
-        "customTnail": tnail_custom.asset->{
-          "url": url,
-          "altText": altText
-        },
-        width,
-        "slug": item->slug,
-        "location": item->location,
-        "title": item->title,
-        "type": item->type,
-        "firstProject": item->projects[0]->{
-          "img": featured.asset->{
-            "url": url,
-            "altText": altText
+         _type == "projectGridItem" => {
+            "_type": item->_type,
+            "_id": item->_id,
+            "featured": item->featured.asset->{
+              "url": url,
+              "alt": altText
+            },
+            "customTnail": tnail_custom.asset->{
+              "url": url,
+              "altText": altText
+            },
+            width,
+            "slug": item->slug,
+            "location": item->location,
+            "title": item->title,
+            "type": item->type,
+            "firstProject": item->projects[0]->{
+              "img": featured.asset->{
+                "url": url,
+                "altText": altText
+              }
+            }
           }
-        }
-      }
       }
   }[0]
-  `
+`;
+
 const { data } = await useSanityQuery(query);
 const activeProjectStore = useActiveProjectStore();
-
 const grid = ref();
 const blurredList = ref(null);
 
-watch(activeProjectStore.activeProject, () => {
-    const id = activeProjectStore.activeProject?._id;
-    if (id) {
-        if (!grid.value) return;
-        blurredList.value = grid.value.querySelectorAll(`li:not([data-title="${id}"])`);
-        if (!blurredList.value) return;
-        blurredList.value.forEach((item) => {
-            item.classList.add('blur');
-        });
-    } else {
-        if (!blurredList.value) return;
-        blurredList.value.forEach((item) => {
-            item.classList.remove('blur');
-        });
-        blurredList.value = null;
-    }
-});
+/** Handles setting the active project */
+function setActiveProject(project) {
+    if (!project?._id) return;
+    activeProjectStore.setActiveProject(project.title, project.location, project._id);
+    applyBlurEffect();
+}
 
-watch(() => activeProjectStore.activeProjectType, () => {
+/** Clears the active project */
+function clearActiveProject() {
+    activeProjectStore.clearActiveProject();
+    removeBlurEffect();
+}
+
+/** Applies the blur effect */
+function applyBlurEffect() {
+    if (!grid.value) return;
+
+    const id = activeProjectStore.activeProject?._id;
     const type = activeProjectStore.activeProjectType;
-    if (type) {
-        if (!grid.value) return;
-        blurredList.value = grid.value.querySelectorAll(`li:not([data-type="${type}"])`);
-        if (!blurredList.value) return;
-        blurredList.value.forEach((item) => {
-            item.classList.add('blur');
-        });
-    } else {
-        if (!blurredList.value) return;
-        blurredList.value.forEach((item) => {
-            item.classList.remove('blur');
-        });
-        blurredList.value = null;
+
+    // Ensure a valid selector is always used
+    if (!id && !type) {
+        removeBlurEffect();
+        return;
     }
-});
+
+    const selector = id
+        ? `li:not([data-title="${id}"])`
+        : `li:not([data-type="${type}"])`;
+
+    blurredList.value = grid.value.querySelectorAll(selector);
+
+    if (!blurredList.value.length) return;
+    blurredList.value.forEach((item) => item.classList.add('blur'));
+}
+
+/** Removes the blur effect */
+function removeBlurEffect() {
+    if (!blurredList.value) return;
+    blurredList.value.forEach((item) => item.classList.remove('blur'));
+    blurredList.value = null;
+}
+
+// Watch for changes and update the blur effect
+watch(() => activeProjectStore.activeProject, applyBlurEffect);
+watch(() => activeProjectStore.activeProjectType, applyBlurEffect);
+onMounted(applyBlurEffect);
 </script>
 
 <style lang="scss" scoped>
--- components/Home/GridFlexItem.vue
+++ components/Home/GridFlexItem.vue
@@ -1,14 +1,13 @@
 <template>
-    <li :class="`unblur`" :data-title="item._id" :data-location="item.location" :data-type="gridItemType"
-        :data-item-width="props.item.width || '35'">
+    <li :class="`unblur`" v-bind="dataAttributes">
         <div v-if="item" class="image-wrapper" @mouseover="setActiveProject" @mouseout="clearActiveProject">
             <template v-if="item._type === 'single'">
-                <img :src="$urlFor(gridImg.url).width(1500).format('webp').url()" :alt="gridImg.alt" />
+                <GridFlexItemImg :src="gridImg.url" :alt="gridImg.alt" />
             </template>
             <template v-else>
                 <NuxtLink v-if="item._type === 'project' || item._type === 'client'"
                     :to="`/${item._type === 'project' ? 'project' : 'client'}/${item.slug?.current}`">
-                    <img :src="$urlFor(gridImg.url).width(1500).format('webp').url()" :alt="gridImg.alt" />
+                    <GridFlexItemImg :src="gridImg.url" :alt="gridImg.alt" />
                 </NuxtLink>
             </template>
         </div>
@@ -16,68 +15,80 @@
 </template>
 
 <script setup>
-import { useActiveProjectStore } from '@/stores/activeProject';
-import { computed } from 'vue';
+import { computed, defineEmits } from 'vue';
+import GridFlexItemImg from '@/components/Home/GridFlexItemImg.vue';
 
-
+const emit = defineEmits(['set-active', 'clear-active']);
 
 const props = defineProps({
     item: Object
 });
 
-const activeProjectStore = useActiveProjectStore();
-
 function setActiveProject() {
-    activeProjectStore.setActiveProject(props.item.title, props.item.location, props.item._id);
+    emit('set-active', {
+        title: props.item.title,
+        location: props.item.location,
+        _id: props.item._id
+    });
 }
 
 function clearActiveProject() {
-    activeProjectStore.clearActiveProject();
+    emit('clear-active');
 }
 
-
 const gridItemType = computed(() => {
-    if (props.item._type === 'single') return 'single';
-    return props.item.type === 'research' ? 'research' : 'commission';
+    switch (props.item._type) {
+        case 'single':
+            return 'single';
+        case 'research':
+            return 'research';
+        case 'commmission':
+            return 'commission';
+        default:
+            return 'commission';
+    }
 });
 
+const dataAttributes = computed(() => ({
+    'data-title': props.item._id,
+    'data-location': props.item.location,
+    'data-type': gridItemType.value,
+    'data-item-width': props.item.width || '35',
+}));
 
 
-
-const gridItemSize = computed(() => {
-    if (gridItemType.value === 'single') {
-        // get custom size,  35vw as fallback
-        const size = props.item.size
-        return size !== undefined ? `${size}vw` : '35vw';
-    }
-
-    if (props.item.width !== undefined) {
-        return `${props.item.width}vw`;
-    }
-    return '35vw';
-});
 const gridImg = computed(() => {
-    let url = '';
-    let alt = '';
-
     if (props.item._type === 'single') {
-        url = props.item.url;
-        alt = props.item?.altText;
-    } else {
-        if (props.item.customTnail) {
-            url = props.item.customTnail.url;
-            alt = props.item.customTnail.altText;
-        } else if (props.item.featured && props.item.featured.url) {
-            url = props.item.featured.url;
-            alt = props.item.customTnail?.alt;
-        } else if (props.item.firstProject && props.item.firstProject.img && props.item.firstProject.img.url) {
-            url = props.item.firstProject.img.url;
-            alt = props.item.firstProject.img.altText;
-        }
+        return {
+            url: props.item.url || '',
+            alt: props.item?.altText || '',
+        };
     }
 
-    return { url, alt };
+    if (props.item.customTnail) {
+        return {
+            url: props.item.customTnail.url,
+            alt: props.item.customTnail.altText,
+        };
+    }
+
+    if (props.item.featured?.url) {
+        return {
+            url: props.item.featured.url,
+            alt: props.item.featured?.alt || '',
+        };
+    }
+
+    if (props.item.firstProject?.img?.url) {
+        return {
+            url: props.item.firstProject.img.url,
+            alt: props.item.firstProject.img.altText || '',
+        };
+    }
+
+    return { url: '', alt: '' };
 });
+
 </script>
 <style lang="scss" scoped>
 li {
